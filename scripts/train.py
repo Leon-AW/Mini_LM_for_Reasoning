3621a246a4c47ea316c2d34516408a442cbd4b60
Leon130996 <Leon130996@github.com>
2024-10-12 09:44:37 +0200

    edited tokenizer

--- scripts/train.py
+++ scripts/train.py
@@ -5,14 +5,37 @@ import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 
-from transformers import Trainer, TrainingArguments, ReformerTokenizerFast
+from transformers import Trainer, TrainingArguments
 from models.reformer_model import ReformerWithCustomEmbeddings
 from utils.data_utils import load_dataset
 import torch
+from gensim.models import Word2Vec
+
+class SimpleTokenizer:
+    def __init__(self, vocab):
+        self.vocab = vocab
+        self.unk_token = "<unk>"
+        self.pad_token = "<pad>"
+        self.eos_token = "</s>"
+
+        # Add special tokens to the vocabulary
+        self.vocab[self.unk_token] = len(self.vocab)
+        self.vocab[self.pad_token] = len(self.vocab)
+        self.vocab[self.eos_token] = len(self.vocab)
+
+    def encode(self, text, max_length=128):
+        tokens = text.split()  # Simple whitespace tokenizer
+        input_ids = [self.vocab.get(token, self.vocab[self.unk_token]) for token in tokens]
+        input_ids = input_ids[:max_length]  # Truncate to max_length
+        input_ids += [self.vocab[self.eos_token]]  # Add EOS token
+        input_ids += [self.vocab[self.pad_token]] * (max_length - len(input_ids))  # Pad to max_length
+        return torch.tensor(input_ids)
+
+    def batch_encode(self, texts, max_length=128):
+        return [self.encode(text, max_length) for text in texts]
 
 def get_reformer_model():
     # Load the trained Word2Vec model
-    from gensim.models import Word2Vec
     word2vec_model = Word2Vec.load("embeddings/wikitext103_word2vec.model")
 
     # Create an embedding matrix
@@ -34,8 +57,10 @@ def train():
 
     model = get_reformer_model().to(device)  # Move model to GPU if available
 
-    # Load a pre-trained fast tokenizer
-    tokenizer = ReformerTokenizerFast.from_pretrained('google/reformer-enwik8')
+    # Initialize the custom tokenizer
+    word2vec_model = Word2Vec.load("embeddings/wikitext103_word2vec.model")
+    vocab = word2vec_model.wv.key_to_index
+    tokenizer = SimpleTokenizer(vocab)
 
     train_dataset, val_dataset = load_dataset(tokenizer)
 
